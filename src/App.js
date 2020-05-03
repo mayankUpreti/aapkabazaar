387ce2ca57c35d1ab4fc9f1fbd75c2af6344c668
Mayank Upreti <mayankupreti27@gmail.com>
2020-05-03 10:40:31 +0530

    After inserting collection item,preview,shoppage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
+
+
+import {Switch,Route}  from 'react-router-dom' 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const HatsPage=(props)=>{
+
+console.log(props)
+return (<div>
+    <h1>Myanak</h1>
     </div>
-  );
+)}
+
+
+function App() {
+  return <div>
+    <Switch>
+   <Route exact path='/' component={HomePage}/>
+   <Route path='/shop' component={ShopPage}/>
+   </Switch>
+  </div>;
+ 
 }
 
 export default App;
