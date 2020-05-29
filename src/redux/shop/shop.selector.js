753ff70287b8833a5dc3763773f57e5d71f982d3
Mayank Upreti <mayankupreti27@gmail.com>
2020-05-29 10:48:20 +0530

    After minor changes in styles

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -2,21 +2,13 @@ import {createSelector} from 'reselect'
 
 const shopSelector= state=>state.shop;
 
-// const COLLECTION_ID_MAP={
-//     hats:1,
-//     sneakers:2,
-//     jackets:3,
-//     womens:4,
-//     mens:5
-// }
-
 
 export const shopSelectorCollections=createSelector(
     [shopSelector],
     shop=>shop.collections
 )
 
-export const  selectCollectionsForPreview=createSelector(
+export const selectCollectionsForPreview=createSelector(
 [shopSelectorCollections],
 collections=>Object.keys(collections).map(key=>collections[key])
 
@@ -25,5 +17,5 @@ collections=>Object.keys(collections).map(key=>collections[key])
 export const selectCollection=collectionUrlParams=>
 createSelector(
     [shopSelectorCollections],
-    collections=>collections[collectionUrlParams]
+    collections=>collections[collectionUrlParams]//dynamically
 )
